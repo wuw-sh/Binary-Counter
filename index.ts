41d4f3bf6da6c47fbf4ba36c6439b1835c726169
wuw.sh <87937525+wuw-sh@users.noreply.github.com>
2022-10-24 03:50:17 +0000

    Initial commit

---
+++ index.ts
@@ -0,0 +1,20 @@
+const bit = 8
+
+function convertToBin(num: number){
+  let arr = [num];
+  while(num / 2 != 0) {
+    num = Math.floor(num / 2);
+    arr.push(num);
+  }
+  arr.pop();
+  arr.reverse();
+  let binary = arr.map(x => x % 2 == 0 ? 0: 1);
+  return Number(binary.join(''))
+}
+for (let i = 0; i < 2 ** bit; i++) {
+  if (i === 0 || i === 1) {
+    console.log("0".repeat(bit - 1) + convertToBin(i))
+    continue
+  }
+  console.log("0".repeat(bit - convertToBin(i).toString().length) + convertToBin(i))
+}
