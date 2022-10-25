7470d6572b4c5985cfc71b264a2d492756b85bb2
wuw.sh <87937525+wuw-sh@users.noreply.github.com>
2022-10-25 01:25:05 +0000

    By wuw.sh

--- .build/index.js
+++ .build/index.js
@@ -1,20 +1,30 @@
-const bit = 8;
-function convertToBin(num) {
-  let arr = [num];
-  while (num / 2 != 0) {
-    num = Math.floor(num / 2);
-    arr.push(num);
+const bit = 5;
+bit < 1 ? console.log("You should input bit value more than 0!") : drawTable();
+function drawTable() {
+  const Length = (value) => value.toString().length;
+  const lastDec = 2 ** bit;
+  const decStr = "Decimal", binStr = "Binary", HexStr = "Hex";
+  const spaceLength = (value1, value2, splitMode = false) => Length(value2) > Length(value1) ? splitMode ? "" : Length(value2) - Length(value1) : "";
+  const splitSpace = (value1, value2) => " ".repeat(spaceLength(value1, value2));
+  console.log(decStr + splitSpace(lastDec, decStr, true) + "|", binStr);
+  let decimal = 0;
+  while (decimal < lastDec) {
+    const Binary = decToBin(decimal);
+    const Hex = decToHex(decimal);
+    const decSpace = " ".repeat(spaceLength(decimal, lastDec));
+    if (decimal === 0 || decimal === 1) {
+      console.log(decSpace + decimal, splitSpace(lastDec, decStr) + "|", "0".repeat(bit - 1) + Binary);
+      decimal++;
+      continue;
+    }
+    console.log(decSpace + decimal, splitSpace(lastDec, decStr) + "|", "0".repeat(bit - Binary.length) + Binary);
+    decimal++;
   }
-  arr.pop();
-  arr.reverse();
-  let binaryDigs = arr.map((x) => x % 2 == 0 ? 0 : 1);
-  return Number(binaryDigs.join(""));
 }
-for (let i = 0; i < 2 ** bit; i++) {
-  if (i === 0 || i === 1) {
-    console.log("0".repeat(bit - 1) + convertToBin(i));
-    continue;
-  }
-  console.log("0".repeat(bit - convertToBin(i).toString().length) + convertToBin(i));
+function decToBin(decimal) {
+  return parseInt(decimal.toString(), 10).toString(2);
+}
+function decToHex(decimal) {
+  return parseInt(decimal.toString(), 10).toString(16);
 }
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,20 +1,32 @@
-const bit = 8 //input value of bit here
+const bit = 5 //input value of bit here
 
-function convertToBin(num: number){
-  let arr = [num];
-  while(num / 2 != 0) {
-    num = Math.floor(num / 2);
-    arr.push(num);
+bit < 1 ? console.log("You should input bit value more than 0!") : drawTable()
+
+function drawTable() {
+  const Length = (value: string | number) => value.toString().length
+  const lastDec = 2 ** bit
+  const decStr = "Decimal", binStr = "Binary", HexStr = "Hex"
+  const spaceLength = (value1: string | number, value2: string | number, splitMode: boolean = false) => Length(value2) > Length(value1) ? (splitMode ? "" : Length(value2) - Length(value1)) : ""
+  const splitSpace = (value1: string | number, value2: string | number) => " ".repeat(spaceLength(value1, value2))
+  console.log(decStr + splitSpace(lastDec, decStr, true) + "|", binStr)
+  let decimal = 0
+  while (decimal < lastDec) {
+    const Binary = decToBin(decimal)
+    const Hex = decToHex(decimal)
+    const decSpace = " ".repeat(spaceLength(decimal, lastDec))
+    if (decimal === 0 || decimal === 1) {
+      console.log(decSpace + decimal, splitSpace(lastDec, decStr) + "|", "0".repeat(bit - 1) + Binary)
+      decimal++
+      continue
+    }
+    console.log(decSpace + decimal, splitSpace(lastDec, decStr) + "|", "0".repeat(bit - Binary.length) + Binary)
+    decimal++
   }
-  arr.pop();
-  arr.reverse();
-  let binary = arr.map(x => x % 2 == 0 ? 0: 1);
-  return Number(binary.join(''))
 }
-for (let i = 0; i < 2 ** bit; i++) {
-  if (i === 0 || i === 1) {
-    console.log("0".repeat(bit - 1) + convertToBin(i))
-    continue
-  }
-  console.log("0".repeat(bit - convertToBin(i).toString().length) + convertToBin(i))
+
+function decToBin(decimal: number) {
+  return parseInt(decimal.toString(), 10).toString(2);
+}
+function decToHex(decimal: number) {
+  return parseInt(decimal.toString(), 10).toString(16);
 }
